05ff836fcd644d564cf40c32f5bee89b4dd00923
Szymon Samus <siizmonsus@gmail.com>
2024-04-08 01:20:36 +0200

    initial commit

--- weather-app/src/components/WeatherBox.jsx
+++ weather-app/src/components/WeatherBox.jsx
@@ -1,16 +1,18 @@
+import CloudyImage from '../assets/cloudy.png';
+
 export default function WeatherBox() {
     return (
-        <div className="flex w-2/4 items-center justify-evenly bg-slate-900 border border-white rounded-md  py-6">
-            <button className="text-white">
-                <img></img>
-                Check Weather
-            </button>
-            <form>
-                <div className="flex flex-col items-center mt-2 mb-2 gap-4">
+        <div className="flex w-1/3 items-center justify-evenly bg-slate-900 border border-white rounded-md  py-6">
+            <form className='w-1/4'>
+                <div className="flex flex-col items-center gap-2">
                 <label className="text-white">Enter Your City</label>
                 <input className="bg-slate-600 rounded-md text-white text-center" type="text" />
                 </div>
             </form>
+            <button className="text-white flex flex-col items-center w-1/4">
+                <img src={CloudyImage} />
+                Check Weather
+            </button>
         </div>
     )
 }
